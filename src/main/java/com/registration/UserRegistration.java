439023a7259d6449f4e7d472325a1cd1ceb19172
unknown <kumar.skm03@gmail.com>
2021-03-05 14:58:49 +0530

    refactor code for lamda expresion is added in UserRegistration.java and corresponding test cases in TestUserRegistration.java

--- src/main/java/com/registration/UserRegistration.java
+++ src/main/java/com/registration/UserRegistration.java
@@ -2,47 +2,55 @@ package com.registration;
 
 import java.util.regex.Pattern;
 
+interface IUserInputIsValid {
+    boolean isValid(String pattern, String userInput);
+}
+
 public class UserRegistration {
 
-    private boolean check;
     private static final String PATTERN_FOR_NAME = "[A-Z]{1}[a-z]{2,}";
     private static final String PATTERN_FOR_EMAIL =  "^[a-zA-z0-9’*+/=?`{|}~^-]{1,}(?:\\.[a-zA-Z0-9’*+/=?`{|}~^-]+)*@[a-zA-Z0-9]{1,}(?:\\.[a-zA-Z,]{2,6})?(?:\\.[a-zA-Z,]{2,6})?$";
     private static final String PATTERN_FOR_PHONE_NUMBER =  "^[0-9]{1,3} [0-9]{10}$";
     private static final String PATTERN_FOR_PASSWORD =  "^^(?=.*[A-Z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,12}$";
 
-    //UC1
-    public void checkFirstName(String firstName) throws UserRegistrationException {
-        if(!Pattern.compile(PATTERN_FOR_NAME).matcher(firstName).matches())
+    IUserInputIsValid checkValue = ((pattern, userInput) -> Pattern.compile(pattern).matcher(userInput).matches());
+
+    public boolean checkFirstName(String firstName) throws UserRegistrationException {
+        if(checkValue.isValid(PATTERN_FOR_NAME, firstName))
+            return true;
+        else
             throw new UserRegistrationException("Invalid First Name");
     }
 
-    //UC2
-    public void checkLastName(String lastName) throws UserRegistrationException {
-        if (!Pattern.compile(PATTERN_FOR_NAME).matcher(lastName).matches())
+    public boolean checkLastName(String lastName) throws UserRegistrationException {
+        if(checkValue.isValid(PATTERN_FOR_NAME, lastName))
+            return true;
+        else
             throw new UserRegistrationException("Invalid Last Name");
     }
 
-    //UC3
-    public void checkEmailId(String email) throws UserRegistrationException {
-        if (!Pattern.compile(PATTERN_FOR_EMAIL).matcher(email).matches())
+    public boolean checkEmailId(String email) throws UserRegistrationException {
+        if(checkValue.isValid(PATTERN_FOR_EMAIL, email))
+            return true;
+        else
             throw new UserRegistrationException("Invalid Email");
     }
 
-    //UC4
-    public void checkPhoneNumber(String phoneNumber) throws UserRegistrationException {
-        if (!Pattern.compile(PATTERN_FOR_PHONE_NUMBER).matcher(phoneNumber).matches())
+    public boolean checkPhoneNumber(String phoneNumber) throws UserRegistrationException {
+        if(checkValue.isValid(PATTERN_FOR_PHONE_NUMBER, phoneNumber))
+            return true;
+        else
             throw new UserRegistrationException("Invalid Phone Number");
     }
 
-    //UC5
-    public void checkPassword(String password) throws UserRegistrationException {
-        if (!Pattern.compile(PATTERN_FOR_PASSWORD).matcher(password).matches())
+    public boolean checkPassword(String password) throws UserRegistrationException {
+        if(checkValue.isValid(PATTERN_FOR_PASSWORD, password))
+            return true;
+        else
             throw new UserRegistrationException("Invalid Password");
     }
 
-    //UC9
     public boolean checkEmail(String email) {
-        check = Pattern.compile(PATTERN_FOR_EMAIL).matcher(email).matches();
-        return check;
+        return Pattern.compile(PATTERN_FOR_EMAIL).matcher(email).matches();
     }
 }
--- src/test/java/com/registration/TestUserRegistration.java
+++ src/test/java/com/registration/TestUserRegistration.java
@@ -14,12 +14,24 @@ public class TestUserRegistration{
     }
 
     @Test
+    public void firstNameTrueTest() throws UserRegistrationException {
+            boolean result = operation.checkFirstName("Shub");
+            Assert.assertTrue(result);
+    }
+
+    @Test
     public void firstNameFalseTest() {
         try {
             operation.checkFirstName("shub");
         } catch (UserRegistrationException e) {
             Assert.assertEquals("Invalid First Name",e.getMessage());
         }
+    }
+
+    @Test
+    public void lastNameTrueTest() throws UserRegistrationException {
+        boolean result = operation.checkLastName("Kub");
+        Assert.assertTrue(result);
     }
 
     @Test
@@ -32,6 +44,12 @@ public class TestUserRegistration{
     }
 
     @Test
+    public void emailTrueTest() throws UserRegistrationException {
+        boolean result = operation.checkEmailId("kumar.skm@gmail.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
     public void emailFalseTest() {
         try {
             operation.checkEmailId("kumar.@.gmail.co.o");
@@ -41,12 +59,24 @@ public class TestUserRegistration{
     }
 
     @Test
+    public void phoneNumberTrueTest() throws UserRegistrationException {
+        boolean result = operation.checkPhoneNumber("91 9111649077");
+        Assert.assertTrue(result);
+    }
+
+    @Test
     public void phoneNumberFalseTest() {
         try {
             operation.checkPhoneNumber("19944556677");
         } catch (UserRegistrationException e) {
             Assert.assertEquals("Invalid Phone Number",e.getMessage());
         }
+    }
+
+    @Test
+    public void passwordTrueTest() throws UserRegistrationException {
+        boolean result = operation.checkPassword("Sh$345hj");
+        Assert.assertTrue(result);
     }
 
     @Test
